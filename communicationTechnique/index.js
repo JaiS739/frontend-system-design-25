7390343df13cdf2dd4809a953ccd0e5784608192
JaiS739 <jksingh00739@gmail.com>
2025-09-06 23:13:14 +0530

    long polling

--- communicationTechnique/index.js
+++ communicationTechnique/index.js
@@ -8,7 +8,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let data = "this is data";
+const clientList = [];
 
+// short polling:-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -17,6 +19,35 @@ app.get("/getData", (req, res) => {
   data = "updated data";
   res.send({
     data,
+  });
+});
+
+// long polling:-
+
+// get the file:-
+
+app.get("/longPolling", (req, res) => {
+  res.sendFile(path.join(__dirname, "longPolling.html"))
+})
+
+app.get("/getPollingData", (req, res) => {
+  if(data!==req.query.lastData){
+    res.json({data});
+  }
+  else{
+    clientList.push(res);
+  }
+});
+
+app.get("/updateData", (req, res) => {
+  data = req.query.data;  // ✅ update global variable
+  while(clientList.length > 0) {
+    const client = clientList.pop();
+    client.json({ data }); // ✅ keep consistent response shape
+  }
+  res.send({
+    message: "data updated successfully",
+    status: 201,
   });
 });
 
