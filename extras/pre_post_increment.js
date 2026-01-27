36467e1348f8bbb528bea6e1c28d6178b03d1718
JaiS739 <jksingh00739@gmail.com>
2026-01-27 18:32:52 +0530

    js: pre and post increment

---
+++ extras/pre_post_increment.js
@@ -0,0 +1,10 @@
+let a = 5;
+
+// post increment
+console.log(a++); // 5 post increment - first print and post increment
+console.log(a); // 6
+
+// comment above one for the result
+// pre increment
+console.log(++a); // 6 increment and print
+console.log(a); // 6
